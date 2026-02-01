42f1fa17d80e54eeb8963702d9914c93e84c938b
vishnupoddar12 <vishnupoddar31@gmail.com>
2026-02-01 20:29:34 +0530

    Improved Lighthouse score of the blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,25 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://your_domain.com', // Required for sitemap
     output: 'static',
+
     image: {
-        service: { entrypoint: 'astro/assets/services/noop' }
+        // service: { entrypoint: 'astro/assets/services/noop' } // Removed to enable default Sharp service
     },
+
+    vite: {
+        build: {
+            minify: 'esbuild', // Ensure minification is enabled
+        },
+    },
+
     prefetch: true,
+
     markdown: {
         shikiConfig: {
             themes: {
@@ -16,4 +28,6 @@ export default defineConfig({
             },
         },
     },
+
+    integrations: [sitemap()],
 });
